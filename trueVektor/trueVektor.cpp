46c8ff4ac05d5c078796525bc6839c8c82814e62
PimonovDaniil <PimonovDaniil@users.noreply.github.com>
2020-09-11 22:09:05 +0700

    Исправил функцию угла

--- trueVektor/trueVektor.cpp
+++ trueVektor/trueVektor.cpp
@@ -38,34 +38,20 @@ double getSinVect(Vector &a, Vector &b) {// нахождение синуса у
     //return multiplyVect(a, b).gedModul() / b.gedModul() / a.gedModul();
 }
 
-double determenant(Vector a, Vector b, Vector c) {
-    return 0.0;
-}
 
-double uglVectGrad(Vector a, Vector b) { //величины угла в градусах между векторами в пределах[–180º, 180º].
-    //return atan2(norm(cross(a, b)), dot(a, b));
-    return atan(getSinVect(a, b)/getCosVect(a, b));
+double uglVectGrad(Vector &a, Vector &b) { //величины угла в градусах между векторами в пределах[–180º, 180º].
+    return atan2(multiplyVect(a, b).gedModul(), scalarMultiply(a, b));
 }
 
 int main()
 {
     setlocale(LC_CTYPE, "RUSSIAN");
-    Vector a(1, 0, 0);
-    Vector b(0, 1, 1);
+    Vector a(1, 1, 0);
+    Vector b(1, 0, 0);
     a.printVector();
     b.printVector();
     printf("угол между a и b %lf\n", uglVectGrad(a, b) * 180 / 3.14159265358979323846);
     printf("угол между b и a %lf\n", uglVectGrad(b, a) * 180 / 3.14159265358979323846);
+    a.printVector();
+    b.printVector();
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-//https://switch-case.ru/70020050
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
